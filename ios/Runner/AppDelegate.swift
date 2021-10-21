39a44c5bb5e9281649162ab02bd8fb960414fa00
Luiz Felipe <felnanuke@gmail.com>
2021-10-21 10:57:35 -0300

    implemented a handle for deeplink on ios for mp

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,45 @@
+// AppDelegate.swift
+
 import UIKit
 import Flutter
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate{
+//  var navigationController: UINavigationController?;
+    var deepLinkMethodChannel:FlutterMethodChannel?;
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+     
+    // Add this line before GeneratedPluginRegistrant
+      let flutterViewController: FlutterViewController = window?.rootViewController as! FlutterViewController;
+      
+      deepLinkMethodChannel = FlutterMethodChannel(name: "deep_link_ios", binaryMessenger: flutterViewController.binaryMessenger)
+      
+      
+    // This line is added by the Flutter App Generator
     GeneratedPluginRegistrant.register(with: self)
+
+    // Add these lines after GeneratedPluginRegistrant
+//    self.navigationController = UINavigationController(rootViewController: flutterViewController);
+//    self.navigationController?.setNavigationBarHidden(true, animated: false);
+//
+//    self.window = UIWindow(frame: UIScreen.main.bounds);
+//    self.window.rootViewController = self.navigationController;
+//    self.window.makeKeyAndVisible();
+    // End of edit
+
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+  override  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      deepLinkMethodChannel?.invokeMethod("url_received", arguments: [
+        "url":url.absoluteString
+      
+        ]);
+        print(url);
+        return true;
+    }
 }
+
