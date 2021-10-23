baf59dc3d76290d830736269090e24b431db972a
Luiz Felipe <felnanuke@gmail.com>
2021-10-23 14:43:14 -0300

    new mp public key add

--- lib/constants/.env.dart
+++ lib/constants/.env.dart
@@ -1,4 +1,4 @@
 const SUPABASE_URL = 'https://exnanxgdrezylebvvgcp.supabase.co';
 const SUPABASE_PUBLIC_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDY4Mzc1MywiZXhwIjoxOTUwMjU5NzUzfQ.FYZqK8x5ELI-6eAiqNET-ZWMxkUAM58uIjDy9W2y2u4';
-const MERCADO_PAGO_PUBLIC_KEY = 'TEST-afa226ca-804c-46c2-bca8-5131e6ef0869';
+const MERCADO_PAGO_PUBLIC_KEY = 'APP_USR-66f22227-9f86-40c9-9efe-85d45fe3eae9';
